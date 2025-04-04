e4e745e0c806110742b163f6cc12bbdf6351d963
ThilagaMC <thilaga2k2@gmail.com>
2025-04-04 18:53:10 +0530

    update backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   
   try {
     const response = await axios.post(
-      "http://localhost:5000/api/contact", 
+      "https://resume-be-ifux.onrender.com/api/contact", 
       formData
     );
     console.log("Response:", response.data);
