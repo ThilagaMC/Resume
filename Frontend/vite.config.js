aeaecbcdb3bcd3ca13de93222fe70dabba92d4b0
ThilagaMC <thilaga2k2@gmail.com>
2025-07-02 15:51:11 +0530

    Vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg'],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // copy to root of dist/
+        }
+      ]
+    })
+  ]
+});
